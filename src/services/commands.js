93265bba065fdfa35814634b26423ad09986a51c
Florian Parret <flo.parret@gmail.com>
2024-07-09 16:14:29 +0200

    feat: refactor commands and tests

--- src/services/commands.js
+++ src/services/commands.js
@@ -1,63 +1,71 @@
 import data from "../../tests/data.js";
 
-const filter = (value) => {
-    const result = [];
-    data.map((country) => {
-      let isContainAnimalValue = false;
-  
-      for (let i = 0; i < country.people.length; i++) {
-        isContainAnimalValue = JSON.stringify(country.people[i].animals)
-          .toLowerCase()
-          .includes(value.toLowerCase());
-        if (isContainAnimalValue) {
-          result.push({ name: country.name, people: [] });
-          break;
-        }
-      }
-      country.people.map((person) => {
-        if (
-          JSON.stringify(person.animals)
-            .toLowerCase()
-            .includes(value.toLowerCase())
-        ) {
-          result[result.length - 1].people.push({
-            name: person.name,
-            animals: [],
-          });
-        }
-        person.animals = person.animals.filter((animal) => {
-          if (animal.name.toLowerCase().includes(value.toLowerCase())) {
-            result[result.length - 1].people[
-              result[result.length - 1].people.length - 1
-            ].animals.push({ name: animal.name });
-          }
+const filter = (valueToFind) => {
+  const result = [];
+  const value = valueToFind.toLowerCase();
+  data.forEach((country) => {
+    const isContainAnimalValue = country.people
+      .flatMap((peopleToFlatmap) => peopleToFlatmap.animals)
+      .find((animal) => animal.name.toLowerCase().includes(value));
+    isContainAnimalValue && result.push({ name: country.name, people: [] });
+
+    country.people.forEach((person) => {
+      const resultToUpdate = result.find(
+        (countryToFind) => countryToFind.name === country.name
+      );
+      if (
+        person.animals.find((animal) =>
+          animal.name.toLowerCase().includes(value)
+        )
+      ) {
+        resultToUpdate.people.push({
+          name: person.name,
+          animals: [],
         });
+      }
+      person.animals
+        .filter((animal) => animal.name.toLowerCase().includes(value))
+        .map((animalToAdd) => {
+          const peopleToUpdate = resultToUpdate.people.find(
+            (peopleToFind) => peopleToFind.name === person.name
+          );
+          peopleToUpdate &&
+            peopleToUpdate.animals.push({ name: animalToAdd.name });
+        });
+    });
+  });
+  console.dir(result, { depth: null, colors: true });
+  return result;
+};
+
+const count = () => {
+  const result = [];
+  data.forEach((country) => {
+    const peopleCount = country.people.length;
+    result.push({ name: country.name + ` [${peopleCount}]`, people: [] });
+
+    country.people.forEach((person) => {
+      const resultToUpdate = result.find(
+        (countryToFind) =>
+          countryToFind.name === country.name + ` [${peopleCount}]`
+      );
+      const animalsCount = person.animals.length;
+      resultToUpdate.people.push({
+        name: person.name + ` [${animalsCount}]`,
+        animals: [],
+      });
+      person.animals.forEach((animalToAdd) => {
+        const peopleToUpdate = resultToUpdate.people.find(
+          (peopleToFind) =>
+            peopleToFind.name === person.name + ` [${animalsCount}]`
+        );
+        peopleToUpdate &&
+          peopleToUpdate.animals.push({ name: animalToAdd.name });
       });
     });
-    console.dir(result, { depth: null, colors: true });
-    return result;
-  };
-  
-  const count = () => {
-      const result = [];
-      data.map((country) => {
-          let peopleCount = country.people.length;
-          result.push({ name: country.name + ` [${peopleCount}]`, people: [] });
-          country.people.map((person) => {
-              let animalsCount = person.animals.length;
-              result[result.length - 1].people.push({
-                  name: person.name + ` [${animalsCount}]`,
-                  animals: [],
-                });
-              person.animals.map((animal) => {
-                  result[result.length - 1].people[
-                      result[result.length - 1].people.length - 1
-                    ].animals.push({ name: animal.name });
-              });
-          });
-      });
-      console.dir(result, { depth: null, colors: true });
-      return result;
-  };
+  });
+  console.dir(result, { depth: null, colors: true });
+  return result;
+};
 
-  export { filter, count };
+export { filter, count };
--- tests/commands.test.js
+++ tests/commands.test.js
@@ -36,32 +36,71 @@ test("Filter command ry have to be as the example", () => {
 test("Count command have to contain elements as the example", () => {
   const dataToFind = [
     {
-      name: 'Dillauti [5]',
+      name: "Dillauti [5]",
       people: [
         {
-          name: 'Winifred Graham [6]',
           animals: [
-            { name: 'Anoa' },
-            { name: 'Duck' },
-            { name: 'Narwhal' },
-            { name: 'Badger' },
-            { name: 'Cobra' },
-            { name: 'Crow' }
-          ]
+            { name: "Anoa" },
+            { name: "Duck" },
+            { name: "Narwhal" },
+            { name: "Badger" },
+            { name: "Cobra" },
+            { name: "Crow" },
+          ],
+          name: "Winifred Graham [6]",
         },
         {
-          name: 'Blanche Viciani [8]',
           animals: [
-            { name: 'Barbet' },
-            { name: 'Rhea' },
-            { name: 'Snakes' },
-            { name: 'Antelope' },
-            { name: 'Echidna' },
-            { name: 'Crow' },
-            { name: 'Guinea Fowl' },
-            { name: 'Deer Mouse' }
-          ]
-        }]}];
+            { name: "Barbet" },
+            { name: "Rhea" },
+            { name: "Snakes" },
+            { name: "Antelope" },
+            { name: "Echidna" },
+            { name: "Crow" },
+            { name: "Guinea Fowl" },
+            { name: "Deer Mouse" },
+          ],
+          name: "Blanche Viciani [8]",
+        },
+        {
+          animals: [
+            { name: "Sand Dollar" },
+            { name: "Buzzard" },
+            { name: "Elephant" },
+            { name: "Xenops" },
+            { name: "Dormouse" },
+            { name: "Anchovy" },
+            { name: "Dinosaur" },
+          ],
+          name: "Philip Murray [7]",
+        },
+        {
+          animals: [
+            { name: "Kowari" },
+            { name: "Caecilian" },
+            { name: "Common Genet" },
+            { name: "Chipmunk" },
+            { name: "Aardwolf" },
+            { name: "Przewalski's Horse" },
+            { name: "Badger" },
+            { name: "Sand Cat" },
+            { name: "Linne's Two-toed Sloth" },
+          ],
+          name: "Bobby Ristori [9]",
+        },
+        {
+          animals: [
+            { name: "Manta Ray" },
+            { name: "Nubian Ibex" },
+            { name: "Warbler" },
+            { name: "Duck" },
+            { name: "Mice" },
+          ],
+          name: "Louise Pinzauti [5]",
+        },
+      ],
+    },
+  ];
   const dataToCount = count();
-  expect(dataToCount).toContain(dataToFind);
+  expect(dataToCount[0]).toEqual(dataToFind[0]);
 });
